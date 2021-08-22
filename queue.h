533353b0e557d58db0eee4eec33ae4210d1c3b87
OnsenSeiya <108993255+OnsenSeiya@users.noreply.github.com>
2021-08-22 21:14:30 +0000

    README typo fix

--- queue.h
+++ queue.h
@@ -3,3 +3,6 @@ typedef struct Queue_ Queue;
 
 Queue *queue_init();
 void queue_enqueue(Queue *q, int elem);
+int *queue_dequeue(Queue *q);
+void queue_free(Queue *q);
+int queue_isemtpty(Queue *q);
